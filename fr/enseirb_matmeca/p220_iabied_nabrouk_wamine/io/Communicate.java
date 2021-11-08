277a2f5bddaa1df19356ad25233bcf10d06c7fa3
ImadABID <imad.abied@gmail.com>
2021-11-08 12:22:06 +0100

    update Communicate

--- fr/enseirb_matmeca/p220_iabied_nabrouk_wamine/io/Communicate.java
+++ fr/enseirb_matmeca/p220_iabied_nabrouk_wamine/io/Communicate.java
@@ -3,16 +3,16 @@ package fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.io;
 import java.util.ArrayList;
 import java.util.List;
 
-import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.Actor;
+import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.WoodPiece;
 import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.cut.Cut;
 
 public class Communicate {
 
     // Input
     
-    static public List<Actor> readClients(String path, String tagName){
+    static public List<WoodPiece> readFromXML(String path, String tagName){
 
-        return new ArrayList<Actor>();
+        return new ArrayList<WoodPiece>();
 
     }
 
