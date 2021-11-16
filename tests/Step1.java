130bfc9beed8699227796d98d00c312d0d6bd282
ImadAbid <imad.abied@gmail.com>
2021-11-16 22:02:55 +0100

    adding test classes for all steps

---
+++ tests/Step1.java
@@ -0,0 +1,19 @@
+package tests;
+
+import java.util.ArrayList;
+
+import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.Board;
+import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.Panel;
+
+public class Step1{
+    public static void main(String[] args) {
+        
+        ArrayList<Panel> panels = Panel.read("fournisseurs.xml");
+        Panel.printList(panels);
+
+        ArrayList<Board> boards = Board.read("clients.xml");
+        Board.printList(boards);
+
+
+    }
+}
