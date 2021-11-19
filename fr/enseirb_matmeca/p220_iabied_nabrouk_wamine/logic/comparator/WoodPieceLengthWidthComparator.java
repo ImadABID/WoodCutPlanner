1b8fc98bd4cb062689797e0faa11931b5aefb0d6
ImadAbid <imad.abied@gmail.com>
2021-11-19 16:39:38 +0100

    adding the Ll & lL comparators

---
+++ fr/enseirb_matmeca/p220_iabied_nabrouk_wamine/logic/comparator/WoodPieceLengthWidthComparator.java
@@ -0,0 +1,23 @@
+package fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.comparator;
+
+import java.util.Comparator;
+
+import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.basic.Rectangle;
+import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.WoodPiece;
+
+public class WoodPieceLengthWidthComparator implements Comparator<WoodPiece> {
+
+    @Override
+    public int compare(WoodPiece w1, WoodPiece w2) throws RuntimeException {
+
+        if( ! (w1.getPolygon() instanceof Rectangle && w2.getPolygon() instanceof Rectangle)){
+            throw new RuntimeException("Comparing two plygones is not implemented.");
+        }
+
+        return Rectangle.compareLexicalOrderLengthWith(
+            (Rectangle) w1.getPolygon(),
+            (Rectangle) w2.getPolygon()
+        );
+    }
+
+}
