4c6b06356bb71e69f78d9b61ff61de168bdcecf2
ImadAbid <imad.abied@gmail.com>
2021-11-27 16:15:58 +0100

    updating Step3Algo2

--- tests/Step3Algo2.java
+++ tests/Step3Algo2.java
@@ -1,19 +1,30 @@
 package tests;
 
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 
+import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.io.Communicate;
 import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.Board;
 import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.Panel;
+import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.cut.Cut;
+import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.cut.CutStep3Algo2;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 
 public class Step3Algo2{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, ParseException, TransformerException, SAXException {
         
         ArrayList<Panel> panels = Panel.read("fournisseurs.xml");
-        Panel.printList(panels);
-
         ArrayList<Board> boards = Board.read("clients.xml");
-        Board.printList(boards);
+        
+        ArrayList<Cut> cuts = new CutStep3Algo2().optimiseCuts(boards, panels);
 
+        Cut.printList(cuts);
+        Communicate.generateCutsXML(cuts);
 
     }
 }
