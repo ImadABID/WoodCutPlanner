51626ab9a44ae56f06e5afe6c810aefe7ccd5ab1
ImadAbid <imad.abied@gmail.com>
2021-11-28 01:10:01 +0100

    issue : Step3.Algo2.isCutPossible always returns false.

--- fr/enseirb_matmeca/p220_iabied_nabrouk_wamine/logic/cut/CutStep3Algo2.java
+++ fr/enseirb_matmeca/p220_iabied_nabrouk_wamine/logic/cut/CutStep3Algo2.java
@@ -7,14 +7,119 @@ import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.basic.Point;
 import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.basic.Rectangle;
 import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.Board;
 import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.Panel;
+import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.comparator.WoodPieceWidthLengthComparator;
 
 public class CutStep3Algo2 implements CutAlgos {
 
-    public ArrayList<Cut> optimiseCuts(List<Board> boards, List<Panel> Panels){
+    public ArrayList<Cut> optimiseCuts(List<Board> boards, List<Panel> panels){
 
-        // To implement later.
+        ArrayList<Cut> cuts = new ArrayList<Cut>();
 
-        return new ArrayList<Cut>();
+
+        panels.sort(new WoodPieceWidthLengthComparator());
+        boards.sort(new WoodPieceWidthLengthComparator());
+
+        Panel panel;
+
+        for(int panels_index = 0; panels_index < panels.size(); panels_index++){
+
+            panel = panels.get(panels_index);
+
+            ArrayList<Cut> pannelCuts = CutStep3Algo2.findBoardsForPanel(panel, (ArrayList<Board>)boards);
+            
+            cuts.addAll(pannelCuts);
+
+
+        }
+
+        return cuts;
+    }
+
+    public static ArrayList<Cut> findBoardsForPanel(Panel panel, ArrayList<Board> boards) throws RuntimeException{
+
+        /*
+         * At the cut, the board has the panel's orientation.
+        */
+
+        ArrayList<Cut> cuts = new ArrayList<Cut>();
+
+        ArrayList<Cut> right_cuts;
+        
+
+        Rectangle panelRect;
+        Rectangle boardRect;
+
+        double currentWidth = 0;
+        Point cut_position;
+        Panel possibleCutZone = panel.deepCopy();
+
+        if(! (possibleCutZone.getPolygon() instanceof Rectangle && panel.getPolygon() instanceof Rectangle)){
+            throw new RuntimeException("Polygon must be a Rectangle.");
+        }
+
+        Rectangle possibleCutZoneRect;
+
+        panelRect = (Rectangle) panel.getPolygon();
+
+        /*
+            Cuts are optimised if their direction is the inverse of the panel's orientation.
+            convention :
+                panel direction horizontal
+                cut direction vertical
+        */
+        panelRect.setOrientation("horizontal");
+
+        Board board;
+        Cut cut;
+
+        for(int bords_index = 0; bords_index < boards.size(); bords_index++){
+
+            board = boards.get(bords_index);
+
+            if( ! (board.getPolygon() instanceof Rectangle)){
+                throw new RuntimeException("Polygon must be a Rectangle.");
+            }
+            boardRect = (Rectangle) board.getPolygon();
+            boardRect.setOrientation("horizontal");
+
+
+            cut_position = new Point(currentWidth, 0);
+
+            possibleCutZoneRect = new Rectangle(
+                cut_position, 
+                panelRect.getDimX()-(cut_position.getX()-panelRect.getLeftTopPt().getX()),
+                panelRect.getDimY()-(cut_position.getY()-panelRect.getLeftTopPt().getY()),
+                true
+            );
+            possibleCutZone.setPolygone(possibleCutZoneRect);
+
+            cut = new Cut(panel, board, cut_position.deepCopy());
+
+            if(CutStep3Algo2.isCutPossible(cut, possibleCutZone) && !board.isPulledOut()){
+
+                board.setAsPulledOut();
+                cuts.add(cut);
+
+                // cut_direction_vertical is vertical
+                cut_position.setX(boardRect.getDimX());
+                possibleCutZoneRect = new Rectangle(
+                    cut_position, 
+                    panelRect.getDimX()-(cut_position.getX()-panelRect.getLeftTopPt().getX()),
+                    boardRect.getDimY(),
+                    true
+                );
+                possibleCutZone.setPolygone(possibleCutZoneRect);
+
+                currentWidth += boardRect.getDimY();
+
+                right_cuts = CutStep3Algo1.findBoardsForPanel(possibleCutZone, boards, panelRect.isVertical());
+
+                cuts.addAll(right_cuts);
+
+            }
+        }
+
+        return cuts;
     }
 
     private static boolean isCutPossible(Cut cut, Panel possibleCutZone){
@@ -52,4 +157,5 @@ public class CutStep3Algo2 implements CutAlgos {
         return cutZoneRect.contains(boardRect);
 
     }
+
 }
