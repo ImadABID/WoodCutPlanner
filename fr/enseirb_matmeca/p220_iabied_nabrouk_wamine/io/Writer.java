4288afb93b2fb9b4cc494dcf32442b278ee92aab
ImadAbid <imad.abied@gmail.com>
2021-12-11 18:55:28 +0100

    Correcting Writable spelling.

--- fr/enseirb_matmeca/p220_iabied_nabrouk_wamine/io/Writer.java
+++ fr/enseirb_matmeca/p220_iabied_nabrouk_wamine/io/Writer.java
@@ -2,7 +2,7 @@ package fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.io;
 
 import java.util.ArrayList;
 
-import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.Writeable;
+import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.Writable;
 
 public interface Writer {
 
