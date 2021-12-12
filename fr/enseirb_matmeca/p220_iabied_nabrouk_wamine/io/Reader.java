cd2b07a8429acdf18955a7c14673d0c70ae166ac
ImadAbid <imad.abied@gmail.com>
2021-12-12 19:25:17 +0100

    Reader Interface

---
+++ fr/enseirb_matmeca/p220_iabied_nabrouk_wamine/io/Reader.java
@@ -0,0 +1,20 @@
+package fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.io;
+
+import fr.enseirb_matmeca.p220_iabied_nabrouk_wamine.logic.Readable;
+
+import java.util.ArrayList;
+
+public interface Reader {
+
+    public static Reader getReader(String type) throws IllegalArgumentException {
+
+        if(type == "XML"){
+            return new XMLReader();
+        }
+        
+        throw new IllegalArgumentException("Uknown type.");
+    }
+
+    public ArrayList<Readable> read(String path);
+
+}
