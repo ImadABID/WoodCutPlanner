2eb432a999f08cdfbb71df87fb286e950efe4704
ImadAbid <imad.abied@gmail.com>
2021-12-16 16:04:47 +0100

    implementing SVG

--- fr/enseirb_matmeca/p220_iabied_nabrouk_wamine/io/SVGWriter.java
+++ fr/enseirb_matmeca/p220_iabied_nabrouk_wamine/io/SVGWriter.java
@@ -7,27 +7,73 @@ import java.util.ArrayList;
 
 class SVGWriter implements Writer{
 
-     public void write(ArrayList<Writable> cuts, String path) {
-         try {
-             ArrayList<String> fields;
-             double width, height;
+    public void write(ArrayList<Writable> cuts, String path) {
+        try {
 
-            for (Writable cut : cuts) {
-                 fields = cut.getFields();
-                 width = Double.parseDouble(fields.get(6));
-                 height = Double.parseDouble(fields.get(7));
+            ArrayList<String> fields_first, fields;
+            String panel_width, panel_height, board_width, board_height, board_x, board_y;
+            Writable cut_first, cut;
 
-                PrintWriter pw = new PrintWriter(path + "_cut_" + cuts.indexOf(cut) + ".svg");
-                pw.println("<svg xmlns=\"http://www.w3.org/2000/svg\"");
-                pw.println("    version=\"1.1\" width=\"" + width + "\" height=\"" + height + "\">");
-                pw.println("<rect width=\"" + width + "\" height=\"" + height + "\" fill=\"orange\" />");
-                pw.println("</svg>");
-                pw.close();
+            PrintWriter pw;
 
-             }
+            ArrayList<Boolean> handled = new ArrayList<Boolean>();
+            for(int i = 0; i < cuts.size(); i++){
+                handled.add(false);
+            }
 
-         }catch(Exception e){
-             System.out.println(e);
-         }
-     }
+            for(int i = 0; i < cuts.size(); i++){
+
+                if(! handled.get(i)){
+                    
+                    handled.set(i, true);
+                    
+                    cut_first = cuts.get(i);
+                    fields_first = cut_first.getFields();
+
+                    board_x = fields_first.get(4);
+                    board_y = fields_first.get(5);
+
+                    panel_width = fields_first.get(6);
+                    panel_height = fields_first.get(7);
+
+                    board_width = fields_first.get(8);
+                    board_height = fields_first.get(9);
+
+                    pw = new PrintWriter(path + "_panel_" + fields_first.get(2) + "." + fields_first.get(3) + ".svg");
+
+                    pw.println("<svg xmlns=\"http://www.w3.org/2000/svg\"");
+                    pw.println("    version=\"1.1\" width=\"" + panel_width + "\" height=\"" + panel_height + "\">");
+                    pw.println("<rect width=\"" + panel_width + "\" height=\"" + panel_height + "\" fill=\"orange\" stroke=\"black\" />");
+
+                    pw.println("<rect x=\""+ board_x +"\" y=\""+ board_y +"\" width=\"" + board_width + "\" height=\"" + board_height + "\" fill=\"red\" stroke=\"black\" />");
+
+                    for(int j = 0; j < cuts.size(); j++){
+                        cut = cuts.get(j);
+                        fields = cut.getFields();
+                        if(! handled.get(j) && (fields_first.get(2) + "." + fields_first.get(3)).equals(fields.get(2) + "." + fields.get(3))){
+                            
+                            handled.set(j, true);
+
+                            board_x = fields.get(4);
+                            board_y = fields.get(5);
+
+                            board_width = fields.get(8);
+                            board_height = fields.get(9);
+
+                            pw.println("<rect x=\""+ board_x +"\" y=\""+ board_y +"\" width=\"" + board_width + "\" height=\"" + board_height + "\" fill=\"red\" stroke=\"black\" />");
+                        }
+                    }
+
+                    pw.println("</svg>");
+                    pw.close();
+
+
+                }
+
+            }
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }
